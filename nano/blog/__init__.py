4f994984dbde7b63853823b08ad9ca1749a70ed9
kaleissin <kaleissin@gmail.com>
2025-09-03 20:34:50 +0200

    Document location of new repo

--- nano/blog/__init__.py
+++
@@ -1,8 +0,0 @@
-from nano.blog.models import Entry as _Entry
-
-def add_entry_to_blog(object, headline, template, date_field=None):
-    data = {'obj': object}
-    template = loader.render_to_string(template, dictionary=data)
-    pub_date = object.__dict__.get(date_field or 'last_modified', datetime.now())
-    blog_entry = _Entry.objects.create(content=template,headline=headline,pub_date=pub_date)
-
--- nano/blog/admin.py
+++
@@ -1,6 +0,0 @@
-
-from django.contrib import admin
-
-from nano.blog.models import *
-
-admin.site.register(Entry)
